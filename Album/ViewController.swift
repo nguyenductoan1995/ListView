ee8827daee6cfe8bbef4b10bec629f2faa928454
nguyenductoann@gmail.com <Toan1995>
2018-04-04 12:58:09 +0700

    Initial Commit

---
+++ Album/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Album
+//
+//  Created by NguyenDucToan on 4/4/18.
+//  Copyright © 2018 NguyenDucToan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
